5a4b9361f066fffdfdbb8a6e3664c174cb3a7eb8
Zoha Iqbal <iqbalza@mail.uc.edu>
2024-11-21 11:49:42 -0500

    method called hello()

---
+++ InClass20241121-IS3050-001/iqbalza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InClass20241121_IS3050_001
+{
+    public class iqbalza
+    {
+        public static void Hello() 
+        {Console.Writeline("OSU will Crush IU")}  
+        
+    }
+}
