b2bdd56cb263770e3f566000a2accda9ae177837
patilsg <patilsg@LCBLAB2110-10>
2024-11-21 11:48:51 -0500

    hello mr. bill

---
+++ InClass20241121-IS3050-001/patilsg.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InClass20241121_IS3050_001
+{
+    public class patilsg
+    {
+        public static void Hello() { Console.Write.line("OSU will crush IU"); }
+    }
+}
