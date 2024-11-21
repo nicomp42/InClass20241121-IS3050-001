7059cf859fc5e8fb197a66113d8264f8e1c3dfc7
Ryan Jacob <jacobry@mail.uc.edu>
2024-11-21 11:49:07 -0500

    Added Hello()

---
+++ InClass20241121-IS3050-001/Jacobry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InClass20241121_IS3050_001
+{
+    public class Jacobry
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("OSU will probably win hopefully");        }
+    }
+}
