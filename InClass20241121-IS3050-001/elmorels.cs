fd39102be337db6e13c36b452b7f444a57bd8a00
Luke Elmore <elmorels@mail.uc.edu>
2024-11-21 11:48:59 -0500

    Elmorels Hello

---
+++ InClass20241121-IS3050-001/elmorels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InClass20241121_IS3050_001
+{
+    public class elmorels
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Osu will crush IU");
+        }
+    }
+}
